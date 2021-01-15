5c758b4c42435e1f5ba2f629bcc9aae46cad9a6c
Kevin <kevinmugiira@gmail.com>
2021-01-16 01:47:09 +0300

    added checkout

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,4 @@ Route::get('/about', [\App\Http\Controllers\AboutController::class, 'index']);
 Route::get('/contact', [\App\Http\Controllers\ContactController::class, 'index']);
 Route::get('/shop',[\App\Http\Controllers\ShopController::class, 'index']);
 Route::get('/payment',[\App\Http\Controllers\PaymentController::class, 'index']);
+Route::get('/checkout',[\App\Http\Controllers\CheckoutController::class, 'index']);
